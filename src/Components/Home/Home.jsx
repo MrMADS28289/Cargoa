979e67cc377df8c6851f625d21591541d2e4064a
ADSM <yesform2018@gmail.com>
2023-05-30 17:23:32 +0600

    Home page completed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,27 @@
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div>
-            home
+        <div className="mx-10">
+            <Link
+                to='/blabla'
+                className="btn btn-block justify-start my-3"
+            >
+                <div className="avatar online me-5">
+                    <div className="w-10 rounded-full">
+                        <img src="/src/assets/meee.png" />
+                    </div>
+                </div> Adam Moon</Link>
+
+            <Link
+                to='/blabla'
+                className="btn btn-block justify-start my-3"
+            >
+                <div className="avatar online me-5">
+                    <div className="w-10 rounded-full">
+                        <img src="/src/assets/meee.png" />
+                    </div>
+                </div> Adam Moon</Link>
         </div>
     );
 };
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
     return (
-        <div>
-            login
-        </div>
+        <>
+            <div className='flex h-screen justify-center items-center'>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="text-center text-2xl font-bold">Login</h2>
+                        <form>
+
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    type="email"
+                                    placeholder="Your Email"
+                                    className="input input-bordered w-full max-w-xs"
+
+                                />
+                                <label className="label">
+                                    <span className="label-text-alt text-red-500">errors.email.message</span>
+                                </label>
+                            </div>
+
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    placeholder="Password"
+                                    className="input input-bordered w-full max-w-xs"
+
+                                />
+                                <label className="label">
+                                    <span className="label-text-alt text-red-500">errors.email.message</span>
+                                </label>
+                            </div>
+
+                            <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
+                        </form>
+                        <p><small>New to DocTime <Link className='text-primary' to="/signup">Create New Account</Link></small></p>
+                        <div className="divider">OR</div>
+                        <button className="btn btn-outline"
+                        >Continue with Google</button>
+                    </div>
+                </div>
+            </div >
+        </>
     );
 };
 
