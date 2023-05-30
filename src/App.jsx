7b22650432c02077c038f712dd01807427deb2fa
ADSM <yesform2018@gmail.com>
2023-05-30 08:24:37 +0600

    Login page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
 import NavBar from './Components/NavBar/NavBar';
+import Registration from './Components/Registration/Registration';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<NavBar />}>
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Registration />} />
+    </Route>
+  )
+)
 
 function App() {
 
   return (
     <>
-      <NavBar />
-
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,17 +1,25 @@
+import { Outlet, Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <nav className="navbar bg-base-300">
-            <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl"> <img className="h-10 rounded-full me-2" src="/src/assets/logo.png" alt="" /> Cargoa
-                </a>
-            </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Login</a></li>
-                </ul>
-            </div>
-        </nav>
+        <>
+            <nav className="navbar bg-base-300">
+                <div className="flex-1">
+                    <Link
+                        to='/'
+                        className="btn btn-ghost hover:bg-base-300 normal-case text-xl"> <img className="h-10 rounded-full me-2" src="/src/assets/logo.png" alt="" /> Cargoa
+                    </Link>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><Link to='login'>Login</Link></li>
+                    </ul>
+                </div>
+
+            </nav>
+            <Outlet />
+        </>
+
     );
 };
 
